6623b92692a3a3fc9f31a0f66c9ccc4e8c3a15dd
Daradege <darg.q.a.a@gmail.com>
2025-03-15 15:25:59 +0330

    merged pyrobale to a single file

--- pyrobale/base.py
+++
@@ -1,6 +0,0 @@
-from .types import (DataBase, ChatMember, CallbackQuery, Chat, User, 
-                   Message, MenuKeyboardMarkup, InlineKeyboardMarkup, InputFile)
-from .exceptions import (BaleAPIError, BaleAuthError, BaleException)
-
-class Client:
-    """Main client class for interacting with Bale API"""
