8f7ab90436eb2b85979d2fc20100e0c54c1d3f3a
Ali Safamanesh <daradege@proton.me>
2025-10-20 19:25:22 +0330

    Updated inputfile.py

--- pyrobale/objects/inputfile.py
+++ pyrobale/objects/inputfile.py
@@ -1,17 +1,34 @@
-from typing import List, Union
+from typing import Dict, Union, Optional
+from io import BufferedReader, BytesIO
 
 
 class InputFile:
-    def __init__(self, file: Union[bytes, str], **kwargs):
-        """
-        Initialize an InputFile object that can handle file uploads in 3 ways:
-        1. Using file_id for files already on Bale servers
-        2. Using HTTP URL for remote files (5MB limit for images, 20MB for other content)
-        3. Using multipart/form-data upload (10MB limit for images, 50MB for other content)
+    def __init__(self, file_input: str | "BufferedReader" | bytes, *, file_name: Optional[str] = None) -> None:
+        if not isinstance(file_input, (str, BufferedReader, bytes)):
+            raise TypeError(
+                "file_input parameter must be one of str, BufferedReader, and byte types"
+            )
 
-        Args:
-            file: Can be either:
-                - bytes: For direct file upload
-                - str: For file_id or HTTP URL
-        """
-        self.file = file
+        if isinstance(file_input, str):
+            file_input = file_input.encode()
+        elif isinstance(file_input, BufferedReader):
+            file_input = file_input.read()
+
+        if file_name:
+            if not isinstance(file_name, str):
+                raise TypeError(
+                    "file_name param must be type of str"
+                )
+
+        self.file_input: Union[bytes, str] = file_input
+        self.file_name: Optional[str] = file_name
+
+    def to_multipart_payload(self) -> Dict:
+        payload = {
+            "value": self.file_input,
+            "content_type": "multipart/form-data"
+        }
+        if self.file_name:
+            payload["filename"] = self.file_name
+
+        return payload
