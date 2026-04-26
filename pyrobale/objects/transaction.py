918819a98f4852f36b6ec82ebb7810dc68951266
Aydin <154819637+Code-Wizaard@users.noreply.github.com>
2026-04-27 01:25:48 +0330

    Fix: Fixed the bale mistakes in documentation

--- pyrobale/objects/transaction.py
+++ pyrobale/objects/transaction.py
@@ -4,9 +4,11 @@ class Transaction:
     status: str,
     userID: int,
     amount: int,
+    provider_payment_charge_id: str,
     createdAt: int):
         self.id = id
         self.status = status
         self.userID = userID
         self.amount = amount
+        self.provider_payment_charge_id = provider_payment_charge_id
         self.createdAt = createdAt
