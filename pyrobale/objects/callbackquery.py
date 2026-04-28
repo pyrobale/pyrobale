15e1d8adfaf6980b88dd05b67684edd3b1cba4f9
Aydin <154819637+Code-Wizaard@users.noreply.github.com>
2026-04-28 17:41:41 +0330

    New Changes ✨ Feat: Added `CallbackQuery.reply()` to directly send message to author of a callback query Feat: made `CallbackQuery.answer()` and `CallbackQuery.reply()` to smart methods Feat: Added `CallbackQuery.is_answerable` property to check if the client is using new bale versions and is it eligible to get callback query answers

--- pyrobale/objects/callbackquery.py
+++ pyrobale/objects/callbackquery.py
@@ -1,5 +1,8 @@
 import typing
-from typing import Optional
+from typing import Optional, Union
+from .utils import smart_method
+from .inlinekeyboardmarkup import InlineKeyboardMarkup
+from .replykeyboardmarkup import ReplyKeyboardMarkup
 
 if typing.TYPE_CHECKING:
     from ..client import Client
@@ -20,7 +23,7 @@ class CallbackQuery:
         self.message = None
         self.chat = None
         self.data = data if data else None
-        self.bot = kwargs.get("client", None)
+        self.bot: Client = kwargs.get("client", None)
 
         if from_user:
             from .user import User
@@ -33,8 +36,21 @@ class CallbackQuery:
             self.message = Message(**message)
             self.chat = self.message.chat if self.message else None
 
+    @smart_method
     async def answer(
         self, text: Optional[str] = None, show_alert: bool = False
     ) -> bool:
         """Sends a response to the callback query."""
         return await self.bot.answer_callback_query(self.id, text, show_alert)
+    
+    @smart_method
+    async def reply(self,
+            text: str,
+            reply_to_message_id: Optional[int] = None,
+            reply_markup: Optional[Union[InlineKeyboardMarkup, ReplyKeyboardMarkup]] = None):
+        """Sends a message to the author of callback query"""
+        return await self.bot.send_message(self.user.id, text, reply_to_message_id=reply_to_message_id, reply_markup=reply_markup)
+    
+    @property
+    def is_answerable(self):
+        return self.data.startswith("1")
